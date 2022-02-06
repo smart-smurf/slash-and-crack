0a3f83bda0f51dfb799b2e38d2f6089b694bbab8
Mina Pêcheux <mina.pecheux@gmail.com>
2022-02-06 21:34:44 +0100

    feat(inputs): allow for left mouse inputs for slash

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,6 +9,7 @@ public class InputManager : MonoBehaviour
     Camera _mainCamera;
 
     private bool _touching;
+    private Vector2 _touchPos;
     private Touch _t;
     private Coroutine _trailingCoroutine;
 
@@ -37,36 +38,54 @@ public class InputManager : MonoBehaviour
         {
             _t = Input.touches[0];
             if (_t.phase == TouchPhase.Began)
-            {
-                if (_t.position.y >= _topUIThreshold) return;
-
-                _trailRenderer.enabled = true;
-                _touching = true;
-                _trail.position = _GetWorldPoint(_t.position);
-                _trailingCoroutine = StartCoroutine(_Trailing());
-            }
+                _PointerDown(_t.position);
             else if (_t.phase == TouchPhase.Moved)
-            {
-                _ray = _mainCamera.ScreenPointToRay(_t.position);
-                if (Physics.Raycast(_ray, out _hit, 100f, _obstacleLayerMask))
-                {
-                    _hit.transform.GetComponent<ObstacleManager>().TakeHit();
-                }
-            }
+                _PointerMove(_t.position);
             else if (_t.phase == TouchPhase.Ended)
-            {
-                _trailRenderer.enabled = false;
-                _touching = false;
-                if (_trailingCoroutine != null)
-                {
-                    StopCoroutine(_trailingCoroutine);
-                    _trailingCoroutine = null;
-                }
-            }
+                _PointerUp();
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+                _PointerDown(Input.mousePosition);
+            else if (Input.GetMouseButton(0))
+                _PointerMove(Input.mousePosition);
+            else if (Input.GetMouseButtonUp(0))
+                _PointerUp();
         }
     }
 
-    private Vector3 _GetWorldPoint(Vector3 screenPoint)
+    private void _PointerDown(Vector2 pos)
+    {
+        if (pos.y >= _topUIThreshold) return;
+
+        _trailRenderer.enabled = true;
+        _touching = true;
+        _touchPos = pos;
+        _trail.position = _GetWorldPoint(pos);
+        _trailingCoroutine = StartCoroutine(_Trailing());
+    }
+
+    private void _PointerMove(Vector2 pos)
+    {
+        _touchPos = pos;
+        _ray = _mainCamera.ScreenPointToRay(pos);
+        if (Physics.Raycast(_ray, out _hit, 100f, _obstacleLayerMask))
+            _hit.transform.GetComponent<ObstacleManager>().TakeHit();
+    }
+
+    private void _PointerUp()
+    {
+        _trailRenderer.enabled = false;
+        _touching = false;
+        if (_trailingCoroutine != null)
+        {
+            StopCoroutine(_trailingCoroutine);
+            _trailingCoroutine = null;
+        }
+    }
+
+    private Vector3 _GetWorldPoint(Vector2 screenPoint)
     {
         Vector3 p = _mainCamera.ScreenToWorldPoint(screenPoint);
         p.z = 0f;
@@ -78,7 +97,7 @@ public class InputManager : MonoBehaviour
         while (true)
         {
             if (_touching)
-                _trail.position = _GetWorldPoint(_t.position);
+                _trail.position = _GetWorldPoint(_touchPos);
             yield return null;
         }
     }
