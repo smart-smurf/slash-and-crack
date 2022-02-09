965af66681bab6e52d57c54b99e8c73fbb92f59a
Mina Pêcheux <mina.pecheux@gmail.com>
2022-02-09 10:04:02 +0100

    fix(deploy): update release script for changelog + release notes

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
-release-it --only-version && cp CHANGELOG.md release_notes.txt
+VERSION=1.0.2
+release-it --only-version && \
+  npx auto-changelog --commit-limit false -u --template keepachangelog && \
+  cp CHANGELOG.md release_notes.txt && \
+  git add CHANGELOG.md release_notes.txt && \
+  git commit -m "chore(release): update changelog + release notes for v${VERSION}" && \
+  git push
