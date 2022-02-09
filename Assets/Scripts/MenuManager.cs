7e54bfa8e8a3de6d6346f2183bccb195ae16b396
Mina Pêcheux <mina.pecheux@gmail.com>
2022-02-09 14:57:00 +0100

    refactor(ui): extract animation + easing to separate class

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -12,10 +9,26 @@ public class MenuManager : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(_BouncingRectTransformSize(_logoShadow, 2, 0.2f));
-        StartCoroutine(_BouncingRectTransformSize(_logo, 2, 0.25f));
-        StartCoroutine(_BouncingRectTransformSize(_titleShadow, 2, 0.4f));
-        StartCoroutine(_BouncingRectTransformSize(_title, 2, 0.45f));
+        StartCoroutine(UIAnimations.ScaleUIElement(
+            _logoShadow,
+            duration: 2,
+            wait: 0.2f,
+            easing: UIAnimations.AnimationEasing.BounceOut));
+        StartCoroutine(UIAnimations.ScaleUIElement(
+            _logo,
+            duration: 2,
+            wait: 0.25f,
+            easing: UIAnimations.AnimationEasing.BounceOut));
+        StartCoroutine(UIAnimations.ScaleUIElement(
+            _titleShadow,
+            duration: 2,
+            wait: 0.4f,
+            easing: UIAnimations.AnimationEasing.BounceOut));
+        StartCoroutine(UIAnimations.ScaleUIElement(
+            _title,
+            duration: 2,
+            wait: 0.45f,
+            easing: UIAnimations.AnimationEasing.BounceOut));
     }
 
     public void Play()
@@ -26,47 +39,5 @@ public class MenuManager : MonoBehaviour
     public void QuitGame()
     {
         SceneBooter.instance.QuitGame();
-    }
-
-    private IEnumerator _BouncingRectTransformSize(
-        RectTransform rectTransform, float delay, float wait = 0,
-        float from = 0f, float to = 1f)
-    {
-        rectTransform.localScale = from * Vector3.one;
-
-        yield return new WaitForSeconds(wait);
-
-        float t = 0f;
-        while (t < delay)
-        {
-            rectTransform.localScale = _BouncingEaseFunction(t / delay) * Vector3.one;
-            t += Time.unscaledDeltaTime;
-            yield return null;
-        }
-
-        rectTransform.localScale = to * Vector3.one;
-    }
-
-    private float _BouncingEaseFunction(float t)
-    {
-        const float n1 = 7.5625f;
-        const float d1 = 2.75f;
-
-        if (t < 1 / d1)
-        {
-            return n1 * t * t;
-        }
-        else if (t < 2 / d1)
-        {
-            return n1 * (t -= 1.5f / d1) * t + 0.75f;
-        }
-        else if (t < 2.5 / d1)
-        {
-            return n1 * (t -= 2.25f / d1) * t + 0.9375f;
-        }
-        else
-        {
-            return n1 * (t -= 2.625f / d1) * t + 0.984375f;
-        }
     }
 }
---
+++ Assets/Scripts/UIAnimations.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UIAnimations
+{
+    public enum AnimationEasing
+    {
+        None,
+        BounceOut,
+    }
+
+    private static Dictionary<AnimationEasing, System.Func<float, float>> _EASING_FUNCS
+        = new Dictionary<AnimationEasing, System.Func<float, float>>()
+    {
+    { AnimationEasing.None, (float t) => t },
+    { AnimationEasing.BounceOut, (float t) => {
+        const float n1 = 7.5625f;
+        const float d1 = 2.75f;
+
+        if (t < 1 / d1) {
+            return n1 * t * t;
+        } else if (t < 2 / d1) {
+            return n1 * (t -= 1.5f / d1) * t + 0.75f;
+        } else if (t < 2.5 / d1) {
+            return n1 * (t -= 2.25f / d1) * t + 0.9375f;
+        } else {
+            return n1 * (t -= 2.625f / d1) * t + 0.984375f;
+        }
+    } },
+    };
+
+    public static IEnumerator ScaleUIElement(
+        RectTransform rect,
+        float from = 0f,
+        float to = 1f,
+        float duration = 1f,
+        float wait = 0f,
+        AnimationEasing easing = AnimationEasing.None)
+    {
+        rect.localScale = new Vector3(from, from, 1);
+
+        if (wait > 0)
+            yield return new WaitForSeconds(wait);
+
+        float t = 0f;
+        float x;
+        System.Func<float, float> f = _EASING_FUNCS[easing];
+        while (t < duration)
+        {
+            x = f(Mathf.Lerp(from, to, t / duration));
+            rect.localScale = new Vector3(x, x, 1);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        rect.localScale = new Vector3(to, to, 1);
+    }
+
+}
