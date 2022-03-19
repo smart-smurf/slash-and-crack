f7565f3255d39b0b4de6073b0df87f788bf6a7a5
Mina Pêcheux <mina.pecheux@gmail.com>
2022-03-19 12:09:39 +0100

    feat(crashlytics): add init+test script

---
+++ Assets/Scripts/CrashlyticsInitializer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Firebase;
+
+public class CrashlyticsInitializer : MonoBehaviour
+{
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // Crashlytics will use the DefaultInstance, as well;
+                // this ensures that Crashlytics is initialized.
+                FirebaseApp app = FirebaseApp.DefaultInstance;
+
+                // Set a flag here for indicating that your project is ready to use Firebase.
+            }
+            else
+            {
+                Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    public void TestCrash()
+    {
+        throw new System.Exception("(ignore) this is a test crash");
+    }
+}
